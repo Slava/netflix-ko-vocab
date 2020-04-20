03badf11f1b614123a1dded9f8286e0115084935
Slava Kim <skim@figma.com>
2020-04-19 18:04:26 -0700

    generate conjugations

--- gen/gen.js
+++ gen/gen.js
@@ -1,4 +1,11 @@
 const fs = require('fs').promises;
+const fetch = require('node-fetch');
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
 
 const dict = {};
 const process = async (fileName, level) => {
@@ -34,11 +41,7 @@ const processBeginner = async () => {
   entries.forEach(entry => dict[entry.word] = entry);
 };
 
-const main = async () => {
-  await process('int2000', 'C');
-  await process('adv2000', 'D');
-  await processBeginner();
-  await fs.writeFile('./dict.json', JSON.stringify(dict, null, 2));
+const collectStats = () => {
   const stats = {
     A: 0,
     B: 0,
@@ -47,6 +50,41 @@ const main = async () => {
   };
   Object.values(dict).forEach(({ level }) => stats[level]++);
   console.log(stats);
+};
+
+const conjugations = async () => {
+  const verbs = Object.keys(dict).filter(word => word.endsWith('다'));
+  const api = 'https://api.verbix.com/conjugator/iv1/ab8e7bb5-9ac6-11e7-ab6a-00089be4dcbc/1/8442/8442/';
+  const conjugationDict = {};
+  let done = 0;
+  for (const word of verbs) {
+    const resp = await (await fetch(api + encodeURI(word))).json();
+    const text = resp.p1.html;
+    const conjugates = text.match(/>(.*)<\/span>/g);
+    conjugates.forEach(dirty => {
+      const clean = dirty.replace(/[^\u3131-\uD79D]/g, '').trim();
+      if (clean === '') {
+        return;
+      }
+      conjugationDict[clean] = word;
+    });
+    done++;
+    if (done % 50 === 0) {
+      console.log(done)
+      // avoid overwhelming the api
+      await sleep(500);
+    }
+  }
+
+  await fs.writeFile('./conjugations.json', JSON.stringify(conjugationDict, null, 2));
+};
+
+const main = async () => {
+  await process('int2000', 'C');
+  await process('adv2000', 'D');
+  await processBeginner();
+  await fs.writeFile('./dict.json', JSON.stringify(dict, null, 2));
+  await conjugations();
 };
 
 main().catch(err => console.warn(err.stack));
